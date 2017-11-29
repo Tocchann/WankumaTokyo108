c9cab27ddba1bc2cde6865cf20e7546c22d12866
高萩 俊行 <tosiyuki@hh.iij4u.or.jp>
2017-11-29 17:43:44 +0900

    プロジェクト ファイルを追加します。

---
+++ DispTargetTree/Program.cs
@@ -0,0 +1,186 @@
+﻿using Microsoft.Build.Construction;
+using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace DispTargetTree
+{
+	class ExecTargetInfo
+	{
+		public ExecTargetInfo()
+		{
+			Name = "";
+			FileName = "";
+			Parent = null;
+			NestLevel = -1;
+			Children = new List<ExecTargetInfo>();
+		}
+		public ExecTargetInfo( ExecTargetInfo parent, string name, string fileName ) : this()
+		{
+			NestLevel = parent.NestLevel + 1;
+			Parent = parent;
+			Name = name;
+			FileName = fileName;
+			parent.Children.Add( this );
+		}
+		public string Name { get; set; }
+		public int NestLevel { get; set; }
+		public string FileName { get; set; }
+		public override string ToString()
+		{
+			var indent = new string( ' ', NestLevel );
+			if( string.IsNullOrWhiteSpace( FileName ) )
+			{
+				return $"{indent}{Name}";
+			}
+			else
+			{
+				return $"{indent}{Name}*{FileName}";
+			}
+		}
+		public ExecTargetInfo Parent { get; set; }
+		public List<ExecTargetInfo> Children { get; private set; }
+	}
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			ProjectTracer.SetupToolset();
+			if( args.Length < 1 )
+			{
+				//	デフォルトのToolset を表示する
+				ProjectTracer.DispDefaultCollection();
+				return;
+			}
+
+			foreach( var arg in args )
+			{
+				var tracer = new ProjectTracer( arg );
+				Console.WriteLine( arg );
+				Trace.WriteLine( arg );
+				Console.WriteLine( "---Imports---" );
+				Trace.WriteLine( "---Imports---" );
+				foreach( var import in tracer.Project.Imports )
+				{
+					Console.WriteLine( import.ImportedProject.FullPath );
+					Trace.WriteLine( import.ImportedProject.FullPath );
+				}
+				Console.WriteLine( "---Properties---" );
+				Trace.WriteLine( "---Properties---" );
+				foreach( var prop in tracer.ProjectInstance.Properties )
+				{
+					Console.WriteLine( $"{prop.Name}=[{prop.EvaluatedValue}]");
+					Trace.WriteLine( $"{prop.Name}=[{prop.EvaluatedValue}]");
+				}
+				//	呼び出しターゲットを階層構造で作り上げる
+				var root = new ExecTargetInfo();
+				//	トップレベルなので、エンプティでいい
+				root.Name = "Root";
+				root.NestLevel = -1;
+				root.Parent = null;
+				var initialTargets = new ExecTargetInfo( root, "InitialTargets", "" );
+				var defaultTargets = new ExecTargetInfo( root, "DefaultTargets", "" );
+				//	まずは、InitialTargets の一覧から呼び出し順を作り上げる
+				foreach( var target in tracer.ProjectInstance.InitialTargets )
+				{
+					AddNewTargets( root, tracer, initialTargets, target );
+				}
+				//	次にビルドターゲットをたどって作り上げる
+				foreach( var target in tracer.ProjectInstance.DefaultTargets )
+				{
+					AddNewTargets( root, tracer, defaultTargets, target );
+				}
+				foreach( var target in tracer.Project.Targets )
+				{
+					if( target.Value.AfterTargetsLocation != null || target.Value.BeforeTargetsLocation != null )
+					{
+						InsertNonSettingTargets( root, tracer, tracer.GetTargetElement( target.Value ) );
+					}
+				}
+				//	ターゲットをドリルダウンしながら表示する
+				var calledTargets = new Dictionary<string, int>();
+				foreach( var target in root.Children )
+				{
+					DispTarget( target, calledTargets );
+				}
+			}
+		}
+		private static void DispTarget( ExecTargetInfo target, Dictionary<string, int> calledTargets )
+		{
+			int callCount;
+			if( calledTargets.TryGetValue( target.Name, out callCount ) == false )
+			{
+				callCount = 0;
+			}
+			callCount++;
+			calledTargets[target.Name] = callCount; ;
+			Console.WriteLine( $"{callCount}.{target}" );
+			Trace.WriteLine( $"{callCount}.{target}" );
+			foreach( var childTarget in target.Children )
+			{
+				DispTarget( childTarget, calledTargets );
+			}
+		}
+		private static void AddNewTargets( ExecTargetInfo root, ProjectTracer tracer, ExecTargetInfo parentTarget, string name )
+		{
+			var target = tracer.ProjectInstance.Targets[name];
+			var newTarget = new ExecTargetInfo( parentTarget, name, target.FullPath );
+			var dependsOn = tracer.ResolveProperties( target.DependsOnTargets );
+			foreach( var depTarget in dependsOn )
+			{
+				AddNewTargets( root, tracer, newTarget, depTarget );
+			}
+		}
+		private static void InsertNonSettingTargets( ExecTargetInfo parentTarget, ProjectTracer tracer, ProjectTargetElement target )
+		{
+			int afterPos = -1;
+			int beforePos = -1;
+			for( int index = 0 ; index < parentTarget.Children.Count ; index++ )
+			{
+				if( parentTarget.Children[index].Name == target.AfterTargets )
+				{
+					afterPos = index;
+					break;
+				}
+			}
+			//	アフターの後に出るオブジェクトを探す
+			for( int index = afterPos+1 ; index < parentTarget.Children.Count ; index++ )
+			{
+				if(parentTarget.Children[index].Name == target.BeforeTargets )
+				{
+					beforePos = index;
+					break;
+				}
+			}
+			//	ここではBeforeの直前に差し込むことを想定して作る(仕様上はどこに入ってもいいことになる)
+			int insPos = -1;
+			if( beforePos != -1 )
+			{
+				//	場所が確定しないので直前に入れておく
+				if( afterPos == -1 )
+				{
+					insPos = beforePos;
+				}
+				else
+				{
+					insPos = afterPos + 1;
+				}
+			}
+			else if( afterPos != -1 )
+			{
+				insPos = afterPos + 1;
+			}
+			if( insPos != -1 )
+			{
+				var insTarget = new ExecTargetInfo( parentTarget, target.Name, target.Location.File );
+			}
+			foreach( var childTarget in parentTarget.Children )
+			{
+				InsertNonSettingTargets( childTarget, tracer, target );
+			}
+		}
+	}
+}
---
+++ DispTargetTree/ProjectTracer.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.Build.Construction;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Execution;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using Microsoft.Win32;
+using System.IO;
+
+namespace DispTargetTree
+{
+	class ProjectTracer
+	{
+		public Project Project { get; private set; }
+		public ProjectInstance ProjectInstance { get; private set; }
+		public ProjectTracer( string projPath )
+		{
+			var col = ProjectCollection.GlobalProjectCollection;
+			Project = col.LoadProject( projPath ); //new Project( projPath, );
+			ProjectInstance = new ProjectInstance( Project.Xml );
+		}
+		internal List<string> ResolveProperties( string value )
+		{
+			//	まず;でパースする
+			var list = value.Split( ';' );
+			//	次に $( で始まって ) で終わるデータをコンバートする
+			var result = new List<string>();
+			foreach( var val in list )
+			{
+				var str = val.Trim();
+				//	コンバートしてリストに突っ込む
+				if( str.StartsWith( "$(" ) && str.EndsWith( ")" ) )
+				{
+					str = str.Substring( 2, str.Length-(2+1) ); //	最初の２文字と最後の１文字をカット
+					str = ProjectInstance.GetPropertyValue( str );
+					var subList = ResolveProperties( str );
+					result.AddRange( subList );
+				}
+				else if( !string.IsNullOrWhiteSpace( str ) )
+				{
+					result.Add( str );
+				}
+			}
+			return result;
+		}
+		internal ProjectTargetElement GetTargetElement( ProjectTargetInstance value )
+		{
+			var importTarget = Project.Imports.Where( im => im.ImportedProject.FullPath == value.FullPath ).First();
+			return importTarget.ImportedProject.Targets.Where( t => t.Name == value.Name ).First();
+		}
+		internal static void SetupToolset()
+		{
+			Console.WriteLine( $"DefaultToolsVersion={ProjectCollection.GlobalProjectCollection.DefaultToolsVersion}" );
+			//	VSは32bitキーしかセットアップしないので、とりあえず３２ビットキーを見る
+			bool setToV15 = false;
+			RegistryKey keyVs7 = null;
+			try
+			{
+				keyVs7 = Registry.LocalMachine.OpenSubKey( @"SOFTWARE\WOW6432Node\Microsoft\VisualStudio\SxS\VS7", false );
+			}
+			catch( Exception )
+			{
+				try
+				{
+					keyVs7 = Registry.LocalMachine.OpenSubKey( @"SOFTWARE\Microsoft\VisualStudio\SxS\VS7", false );
+				}
+				catch( Exception )
+				{
+				}
+			}
+			if( keyVs7 != null )
+			{
+				try
+				{
+					var path = keyVs7.GetValue( "15.0" ) as string;
+					var toolsetPath = Path.Combine( path, @"MSBuild\15.0\Bin" );
+					if( Directory.Exists( toolsetPath ) )
+					{
+						ProjectCollection.GlobalProjectCollection.AddToolset( new Toolset( "15.0", toolsetPath, ProjectCollection.GlobalProjectCollection, toolsetPath ) );
+						ProjectCollection.GlobalProjectCollection.DefaultToolsVersion = "15.0";
+						setToV15 = true;
+					}
+				}
+				catch( Exception )
+				{
+				}
+			}
+			//	最新バージョンをセットしていない場合は、リストの一番新しいやつを探す
+			if( !setToV15 )
+			{
+				string targetToolset = "";
+				Version prevVer = new Version( ProjectCollection.GlobalProjectCollection.DefaultToolsVersion );
+				foreach( var toolset in ProjectCollection.GlobalProjectCollection.Toolsets )
+				{
+					var toolsetVer = new Version( toolset.ToolsVersion );
+					if( prevVer < toolsetVer )
+					{
+						targetToolset = toolset.ToolsVersion;
+						prevVer = toolsetVer;
+					}
+				}
+				if( !string.IsNullOrWhiteSpace( targetToolset ) )
+				{
+					ProjectCollection.GlobalProjectCollection.DefaultToolsVersion = targetToolset;
+				}
+			}
+			Console.WriteLine( $"DefaultToolsVersion={ProjectCollection.GlobalProjectCollection.DefaultToolsVersion}" );
+		}
+		internal static void DispDefaultCollection()
+		{
+			foreach( var toolset in ProjectCollection.GlobalProjectCollection.Toolsets )
+			{
+				Console.WriteLine( $"ToolsVersion={toolset.ToolsVersion}" );
+				Console.WriteLine( $"ToolsVersion={toolset.ToolsPath}" );
+				if( toolset.Properties.Count != 0 )
+				{
+					Console.WriteLine( "--Properties--" );
+					foreach( var propPair in toolset.Properties )
+					{
+						Console.WriteLine( $"  {propPair.Key}=[{propPair.Value.EvaluatedValue}]");
+					}
+				}
+				if( toolset.SubToolsets.Count != 0 )
+				{
+					Console.WriteLine( "--SubToolset--" );
+					foreach( var subSet in toolset.SubToolsets )
+					{
+						Console.WriteLine( $"  SubToolsetVersion={subSet.Value.SubToolsetVersion}" );
+						Console.WriteLine(  "  --Properties--" );
+						foreach( var propPair in subSet.Value.Properties )
+						{
+							Console.WriteLine( $"    {propPair.Key}=[{propPair.Value.EvaluatedValue}]" );
+						}
+					}
+				}
+				Console.WriteLine("---");
+			}
+		}
+	}
+}
